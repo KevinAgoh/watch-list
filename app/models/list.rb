a408abc72e63565dae325053a8677015a4600c38
Kevin Agoh <kevinagoh@hotmail.fr>
2021-04-13 14:30:14 +0200

    validations done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 
-  has_many :bookmarks, dependant: :destroy
+  has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 end
