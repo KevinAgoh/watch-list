22d81650ad422b50fbefdd83655ac1fa78e3b815
Kevin Agoh <kevinagoh@hotmail.fr>
2021-05-25 23:04:35 +0200

    can't solve the comment issue

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,24 +1,18 @@
 class BookmarksController < ApplicationController
-
-  def show
-    @bookmark = Bookmark.find(params[:id])
+  def new
+    @bookmark = Bookmark.new
   end
 
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    if @bookmark.save
-      redirect_to list_path(@list)
-    else
-      render 'lists/show'
-    end
+    flash[:notice] = @bookmark.errors.full_messages.to_sentence unless @bookmark.save
+    redirect_to list_path(@list)
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-
     redirect_to list_path(@bookmark.list)
   end
 
