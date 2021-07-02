c0c55f7db8242e46650a2f9c98358c7abc192d86
Kevin Agoh <kevinagoh@hotmail.fr>
2021-07-02 17:11:02 +0200

    found the bug. Now the comment displays in the card

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [ :index, :show, :new, :create ] do
-    resources :bookmarks, only: [ :create, :new, :destroy ]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[create new]
   end
+  resources :bookmarks, only: :destroy
 end
