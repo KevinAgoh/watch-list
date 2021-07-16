e95000d485ee9c8dce30d61e02dc76c2a7eaec52
Kevin Agoh <kevinagoh@hotmail.fr>
2021-07-16 16:15:53 +0200

    solved the delete bug

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,7 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list)
   end
